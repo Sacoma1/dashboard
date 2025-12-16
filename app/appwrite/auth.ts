d9236e63f4ccea96c5685a51d4566226f4e029af
sacoma <msantes15@gmail.com>
2025-12-15 21:18:27 -0600

    updates

---
+++ app/appwrite/auth.ts
@@ -0,0 +1,43 @@
+export const loginWithGoogle = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+
+
+export const logoutUser = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getUser = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getGoglePicture = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const storeUserData = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getExistingUser = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
